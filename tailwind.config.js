f2a96308675d05614cea3341978ad8435aff8018
eshamah02 <eshamah02@gmail.com>
2024-09-09 00:12:36 -0400

    reorganizing file structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{html,js}",  // Adjust this path to your project structure
+    "./**/*.{html,js}",  // Adjust this path to your project structure
   ],
   theme: {
     extend: {
